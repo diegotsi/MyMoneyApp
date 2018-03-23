a1bced97b1e87fc0a51c7e169ae7015deb093090
diegotsi <diegooliveira.tsi@gmail.com>
2018-03-23 18:01:56 -0300

    Inclusão com o redux, e usando a api

--- src/common/template/footer.jsx
+++ src/common/template/footer.jsx
@@ -4,7 +4,7 @@ export default props => (
     <footer className='main-footer'>
         <strong>
             Copyright &copy; 2018
-            <a href='http://diegotsi.github.io'>Diego Oliveira</a>
+            <a href='http://diegotsi.github.io'> Diego Oliveira</a>
         </strong>
     </footer>
 )
---
+++ src/main/reducers.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+import dashboard from '../dashboard/reducer'
+
+const rootReducer = combineReducers({
+    dashboard
+})
+
+export default rootReducer
