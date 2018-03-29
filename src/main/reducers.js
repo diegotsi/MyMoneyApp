062a0adfc1389765f90f214534b83735f0c50f15
diegotsi <diegooliveira.tsi@gmail.com>
2018-03-29 18:36:44 -0300

    Abas conectadas ao redux

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import dashboard from '../dashboard/reducer'
+import tab from '../common/tab/tabReducer'
 
 const rootReducer = combineReducers({
-    dashboard
+    dashboard,
+    tab
 })
 
 export default rootReducer
